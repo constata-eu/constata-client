eaca6a9ea98ff8985c6991b1b07c95719bead9f9
bronxi <57157439+bronxi47@users.noreply.github.com>
2021-10-20 16:58:27 -0300

    Update correct output

--- How to's/constata-multistamp/constata-multistamp.sh
+++ How to's/constata-multistamp/constata-multistamp.sh
@@ -5,5 +5,5 @@ cliPass=$2
 stampFiles=$(ls $stampPath)
 line=document_id
 for i in $stampFiles; do
-    ./constata-cli-linux --password $cliPass stamp $stampPath/"$i" | ( grep "$line" -m 1 | tr -d '\n "document_id":'; echo "$i" ) >> $1.csv;
+    ./constata-cli-linux --password $cliPass stamp $stampPath/"$i" | ( grep "$line" -m 1 | sed 's/"document_id"://' | tr -d '\n "'; echo "$i" ) >> $1.csv;
 done
