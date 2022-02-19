7d4897ac7a43043f77da3571da9b98b346b97fd0
bronxi47 <hbenja47@gmail.com>
2022-02-19 10:56:17 -0300

    Add normalization of files with spaces in the name & Add files processing counter

--- How to's/constata-multistamp/constata-multistamp.sh
+++ How to's/constata-multistamp/constata-multistamp.sh
@@ -2,8 +2,15 @@
 
 stampPath=$(pwd)/$1
 cliPass=$2
-stampFiles=$(ls $stampPath)
+ls $stampPath | while read -r FILE; do [[ "$FILE" == *" "* ]] && mv $stampPath/"$FILE" $stampPath/"${FILE// /_}" ; done
 line=document_id
+stampFiles=$(ls $stampPath)
+total=$(echo $stampFiles | wc -w)
+count=1
+
 for i in $stampFiles; do
-    ./constata-cli-linux --password $cliPass stamp $stampPath/"$i" | ( grep "$line" -m 1 | sed 's/"document_id"://' | tr -d '\n "'; echo "$i" ) >> $1.csv;
+  printf "\rProcesando %5d/%-5d %-60s" $count $total $i
+  ./constata-cli-linux --password $cliPass stamp $stampPath/"$i" | ( grep "$line" -m 1 | sed 's/"document_id"://' | tr -d '\n "'; echo "$i" ) >> $1.csv;
+  let "count=count+1"
 done
+printf "\nFinalizado\n"
