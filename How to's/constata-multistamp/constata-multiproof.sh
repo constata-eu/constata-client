6068164ee3e013e763deeb928e776d52ff4c63dd
bronxi47 <hbenja47@gmail.com>
2022-02-21 10:15:59 -0300

    Change output directory name to certificados_'csv_name'

--- How to's/constata-multistamp/constata-multiproof.sh
+++ How to's/constata-multistamp/constata-multiproof.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+dirName=${1%????}
 csvPath=$(pwd)/$1
 cliPass=$2
-folder=certificados_$(date +%d-%m-%Y-%T)
+folder=certificados_$dirName
 total=$(cat $csvPath | wc -l)
 count=1
 
@@ -16,13 +16,14 @@ fi
 while IFS=, read col1 col2
 do
 	state=$(./constata-cli-linux --password $cliPass show ${col1} | ( grep state -m 1 | sed 's/"state"://' | tr -d '\n ",'))
+	state="${state,,}"
 	if [ $state == "published" ]; then
 		[ $count -eq 1 ] && mkdir $folder && echo "Directorio de destino:" $folder
-		printf "\rProcesando %5d/%-5d %-60s" $count $total "Documento: "${col2}
+		printf "\rProcesando %5d/%-5d %-50s" $count $total "Documento: "${col2}
 		./constata-cli-linux --password $cliPass fetch-proof ${col1} > $folder/"${col2}".html
 		let "count=count+1"
 	else
-		printf "\rDocumento NO publicado, intentelo más tarde | %-60s " ${col2}
+		printf "\rDocumento NO publicado, intentelo más tarde | %-50s " ${col2}
 	fi
 done < $csvPath
 printf "\nFinalizado\n"
